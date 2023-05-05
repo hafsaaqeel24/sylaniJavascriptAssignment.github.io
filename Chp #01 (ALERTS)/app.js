08f42de8ef5c7e17eaf04a03e8c49757be0f6281
hafsaaqeel24 <aqeelhafsa572@gmail.com>
2023-05-06 02:08:27 +0500

    java script assignment of sylani institute

---
+++ Chp #01 (ALERTS)/app.js
@@ -0,0 +1,19 @@
+// question #01 
+// alert ("Hey");
+
+// question #02
+// alert ("Error! Please enter a valid password.");
+
+// question #03
+// alert ("Welcomr to JS Land...\nHappy Coding");
+
+// question #04  
+// alert ("Welcomr to JS Land...");
+// alert ("Happy Coding");
+
+// question #05
+// alert ("Hello... I can run JS through my web browser's console");
+// console.log(alert);
+
+// question #06
+// question #07
---
+++ Chp #02 (VARIABLES FOR STRINGS)/app.js
@@ -0,0 +1,40 @@
+// question #01 
+// let userName;
+
+// question #02
+// let myName = "Hafsa Aqeel";
+// document.write(myName) ;
+
+// question #03
+// let message = "Hello World";
+// alert (message);
+
+// question #04
+// let theName = "John Doe"
+// let age = "15 years old"
+// let skill = "Certified Mobile Application Development"
+// alert (theName);
+// alert (age);
+// alert (skill);
+
+// question #05
+// let pizza = "PIZZA\nPIZZ\nPIZ\nPI\nP"
+// alert (pizza);
+
+// question #06
+// let email = " aqeelhafsa572@gmail.com"
+// alert ("My Email Address is" + email)
+
+// question #07
+// let book = " A smarter\nway to learn JavaScript"
+// alert ("I am trying to learn from the Book" + book)
+
+// question #08
+// let theHtml = "Yah! I can write HTML content through JavaScript"
+// document.write (theHtml);
+
+// question #09
+// let symbols = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬"
+// alert (symbols);
+
+
